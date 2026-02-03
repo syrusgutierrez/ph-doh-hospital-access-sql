56168902be51ce6d6a53c8d01764a22e572791b1
Syrus Mac Gutierrez <143099169+syrusgutierrez@users.noreply.github.com>
2026-02-03 21:14:44 +0800

    Fix GROUP BY clause in hospitals query

--- sql/02_hospitals_per_million.sql
+++ sql/02_hospitals_per_million.sql
@@ -1,7 +1,8 @@
 SELECT 
     ddh.region,
-    (COUNT(ddh.facility) * 1000000 / dpr.total_population)
+    dpr.total_population,
+    ROUND(COUNT(ddh.facility) * 1000000.0 / dpr.total_population, 2) AS hospitals_per_million
 FROM dim_doh_hospital ddh 
 JOIN dim_population_region dpr 
     ON ddh.region = dpr.region
-GROUP BY 1;
+GROUP BY ddh.region, dpr.total_population;
