483aa4c03e9fed466db77cbf00c9caa633f40350
Syrus Mac Gutierrez <143099169+syrusgutierrez@users.noreply.github.com>
2026-02-03 21:15:24 +0800

    Update 03_below_national_average.sql

--- sql/03_below_national_average.sql
+++ sql/03_below_national_average.sql
@@ -1,25 +1,20 @@
 WITH hospital_rate AS (
     SELECT 
         ddh.region,
-        (COUNT(ddh.facility) * 1000000 / dpr.total_population) AS hospitals_per_million
+        ROUND(COUNT(ddh.facility) * 1000000.0 / dpr.total_population, 2) AS hospitals_per_million
     FROM dim_doh_hospital ddh 
     JOIN dim_population_region dpr 
         ON ddh.region = dpr.region
-    GROUP BY 1, dpr.total_population
+    GROUP BY ddh.region, dpr.total_population
 ),
-
 national_avg AS (
     SELECT
-        AVG(hr.hospitals_per_million) AS avg_hospitals_per_million
-    FROM hospital_rate hr
+        (ROUND(AVG(hospitals_per_million), 2)) AS avg_hospitals_per_million
+    FROM hospital_rate
 )
-
 SELECT
     hr.region,
-    hr.hospitals_per_million,
-    na.avg_hospitals_per_million
-FROM hospital_rate hr 
+    hr.hospitals_per_million
+FROM hospital_rate hr
 JOIN national_avg na 
-    ON hr.hospitals_per_million < na.avg_hospitals_per_million
-ORDER BY 2 ASC
-LIMIT 1;
+    ON hr.hospitals_per_million < na.avg_hospitals_per_million;
